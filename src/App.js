c16341251dfb0d371ae2bbf58afa17d3141b717f
harshm236 <harshm236@gmail.com>
2022-11-01 19:33:53 +0530

    feedbackform

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-function App() {
+
+const arr = []
+export function App() {
+  function Submission(Event) {
+    Event.preventDefault()
+     const z = {}
+     z.name=document.getElementById("name").value;
+     z.dept=document.getElementById("dept").value;
+     z.rate=document.getElementById("rate").value;
+
+     arr.push(z)
+     console.log(arr)
+
+
+    const x = ReactDOM.createRoot(document.getElementById("root2"));
+    x.render(
+      <>
+
+        <div >
+          <div id="card">{arr.map(a=><div>name-{a.name}<br/> department-{a.dept}<br/> rating-{a.rate}<hr/></div>)}</div>
+       
+         </div> 
+      </>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        textAlign: "center",
+        fontSize: "30px",
+        alignItems: "center",
+      }}
+    >  <form action="">
+      <h1 style={{ textAlign: "center" }}>EMPLOYEE FEEDBACK FORM</h1>
+      <span>NAME-</span> 
+      <input name="name" id="name"  type={"text"} />
+      <hr />
+      <span>DEPARTMENT-</span>
+      <input name="dept" id="dept"  type={"text"} />
+      <hr />
+      <span>RATING-</span>
+      <input name="rate" id="rate"  type={"number"} />
+      <hr />
+      <input onClick={Submission} type={"submit"}></input>
+      </form>
     </div>
   );
 }
-
-export default App;
